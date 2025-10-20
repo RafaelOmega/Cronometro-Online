8c0d0a7067a1692f22143b80bb6df1cf6d938029
RafaelOmega <raffael.oliveira.823@gmail.com>
2025-10-20 00:26:33 -0300

    Hora atualizando

--- scripts.js
+++ scripts.js
@@ -1,2 +1,51 @@
-const elemExercicio = document.querySelector(".exercicio");
+const elemsExercicio = document.querySelectorAll(".exercicio");
 const elemTela = document.querySelector(".tela");
+const elemVoltar = document.querySelector(".voltar");
+
+const elemClose = document.querySelector(".close");
+const elemPlay = document.querySelector(".play");
+const elemPause = document.querySelector(".pause");
+
+const elemCronometro = document.querySelector(".cronometro");
+
+const elemHorario = document.querySelector(".horario");
+
+// ========= Adicionar classe tela--cronometro =========
+elemsExercicio.forEach(function (elemExercicio) {
+  elemExercicio.addEventListener("click", function () {
+    elemTela.classList.add("tela--cronometro");
+  });
+});
+
+elemVoltar.addEventListener("click", function () {
+  elemTela.classList.remove("tela--cronometro");
+});
+
+// ========= Adicionar classe iniciou =========
+elemClose.addEventListener("click", function () {
+  elemCronometro.classList.remove("iniciou");
+});
+
+elemPlay.addEventListener("click", function () {
+  elemCronometro.classList.add("iniciou");
+});
+
+elemPause.addEventListener("click", function () {
+  elemCronometro.classList.remove("iniciou");
+});
+
+// ========= Atualizar horario =========
+function atualizarHorario() {
+  const horas = new Date().getHours().toString().padStart(2, "0");
+  const minutos = new Date().getMinutes().toString().padStart(2, "0");
+  const horario = horas + ":" + minutos;
+
+  elemHorario.innerText = horario;
+  console.log(horario);
+}
+
+atualizarHorario();
+
+setInterval(() => {
+  atualizarHorario();
+}, 5000);
